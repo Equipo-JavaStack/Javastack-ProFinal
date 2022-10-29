af41839f30936ac6c6745e483e107925b9134107
Luis Bautista <luisbauistareyes@gmail.com>
2022-10-29 00:43:29 -0500

    Se sube proyecto a Master

---
+++ hoppybeerapp/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+// import {Inventario} from './Components/Inventario';
+import {Navbar} from './Components/Navbar';
+
+
+export const App = () => {
+  return (
+    <>
+    <header>
+    
+    <Navbar/>
+    </header>
+    </>
+  )
+}
